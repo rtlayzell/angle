981abd1a64e07cb33202b9a4159a6b11f401be0c
Reegan Layzell <reegan.layzell@live.co.uk>
2016-01-11 10:10:50 +0000

    Stub function for the vector cross product

--- include/math/vector.hpp
+++ include/math/vector.hpp
@@ -228,6 +228,13 @@ namespace math {
 
 	////////////////////////////////////////////////////////////////////////////////
 
+	template <typename _T, typename _U>
+	vector<typename std::common_type<_T, _U>::type, 3> cross_product(vector<_T, 3> const& lhs, vector<_U, 3> const& rhs) {
+		typedef typename std::common_type<_T, _U>::type common_t;
+		throw "not implemented";
+		return vector<common_t, 3> { };
+	}
+
 	template <typename _T, typename _U, std::size_t _N>
 	typename std::common_type<_T, _U, float>::type dot_product(vector<_T, _N> const& lhs, vector<_U, _N> const& rhs) {
 		typedef typename std::common_type<_T, _U, float>::type common_t;
